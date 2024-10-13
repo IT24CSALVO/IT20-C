eab370be84a28231974f94d1ef7b8ce3064ae7ee
IT24CSALVO <alliahshanesalvo11@gmail.com>
2024-10-13 11:28:24 +0800

    Added size method to display values

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -27,6 +27,10 @@ public class Main {
 	            	 System.out.println("Add a number: ");
                      int num = sc.nextInt();
                      System.out.println("Pushed value: " + num);
+                     stack.push(num);
+                     stack.size();
+                     System.out.println("~~~~~~~~~~~~~~~~~~");
+                     break; 
 }
 }
 }
